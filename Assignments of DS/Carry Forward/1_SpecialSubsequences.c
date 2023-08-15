919ea78c4277c8c59a6f3517942b8c481c95f82a
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-08-15 21:10:33 +0530

    Special Subsequence

---
+++ Assignments of DS/Carry Forward/1_SpecialSubsequences.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int specialSubsequence(char *str) {
+	int size = sizeof(str)/sizeof(char);
+
+	for(int i = 0; i < size; i++) {
+		
+		for(int j = i + 1; ) {
+		}
+	}
+}
+void main() {
+	
+	char *str = "ABCGAG";
+}
